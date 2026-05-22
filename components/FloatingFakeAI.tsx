186928df4b79c21279fef0a706b1606b8f64d045
braxtonvogel <braxtonvogel.w4ves@gmail.com>
2026-05-21 20:03:35 -0500

    fix fakeAI import resolution

--- components/FloatingFakeAI.tsx
+++ components/FloatingFakeAI.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { searchSkills } from "@/lib/fakeAI";
+import { searchSkills } from "../lib/fakeAI";
 
 export default function FloatingFakeAI() {
   const [open, setOpen] = useState(false);
